a4b028fad6f9233fee339b8dfb605a37069db049
Mario Puebla <mario.puebla@rmcbrothers.com.br>
2020-03-20 20:15:16 -0300

    first commit

---
+++ site/sistema/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { logWarnings } from 'protractor/built/driverProviders';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'sistema';
+}
